e23081a592e2fff17f4b94d444367d55b20a0e41
T0RCHW00D <twilson170@gmail.com>
2020-10-13 17:19:41 +1100

    First Push
    Bad movement, bad UI, no UI interaction, no sprite flipping

---
+++ Assets/Scripts/Char/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Vertical") > 0.05 || Input.GetAxis("Vertical") < -0.05)
+        {
+            gameObject.transform.position += new Vector3(0.0f, 0.1f, 0.0f) * Input.GetAxis("Vertical");
+        }
+        if(Input.GetAxis("Horizontal") > 0.05 || Input.GetAxis("Horizontal") < -0.05)
+        {
+            gameObject.transform.position += new Vector3(0.1f, 0.0f, 0.0f) * Input.GetAxis("Horizontal");
+        }
+    }
+}
