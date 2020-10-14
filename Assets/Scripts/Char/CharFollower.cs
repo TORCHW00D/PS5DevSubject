e3405e7aa4ec1d02989bed10c8cff014d0be83df
T0RCHW00D <twilson170@gmail.com>
2020-10-15 10:38:58 +1100

    Tile map added
    Created simple tilemap sprite ssytem, don't have collision / hit detection working yet -- need info from Unity website / tutorials to get surfaces working properly.

---
+++ Assets/Scripts/Char/CharFollower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharFollower : MonoBehaviour
+{
+    public GameObject CharToFollow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        gameObject.transform.position = new Vector3( CharToFollow.transform.position.x, CharToFollow.transform.position.y, -10.0f);
+        
+    }
+}
