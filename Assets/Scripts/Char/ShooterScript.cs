465c3a890357f43798552f9a0fefbefd0e90ea98
T0RCHW00D <twilson170@gmail.com>
2020-10-15 11:01:40 +1100

    [bugfix] Stopped map from falling off screen
    Oops. Also added outline for shooter script.

---
+++ Assets/Scripts/Char/ShooterScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterScript : MonoBehaviour
+{
+    /*
+     * Flatten 2nd stick input to Normalized 2D vector & fire in that direction centered off player
+     * Snap to aim sounds F U N
+     */
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
